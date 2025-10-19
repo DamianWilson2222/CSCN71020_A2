e136ec01f9fa4fa06e12548193fe9f61dd933b4d
DamianWilson2222 <dwilson9978@conestogac.on.ca>
2025-10-19 05:58:09 -0400

    Add comments to Task1 TEST Class

--- BCSRec.Tests/BCSRec.Tests.cpp
+++ BCSRec.Tests/BCSRec.Tests.cpp
@@ -11,10 +11,13 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace BCSRecTests
 {
+	//Name for the testing section followed by the type of functions
 	TEST_CLASS(Task1_RectangleCalculator)
 	{
 	public:
 		
+		//This function validates the getPerimeter() using a length of 10 and a width of 3 (within the range)
+		//This function is expected to use the formula 2*(length+width)=perimeter or 2*(10+3)=26
 		TEST_METHOD(PerimeterFunctionality_Length10Width3_Returns26)
 		{
 			int result = 0;
@@ -25,6 +28,8 @@ namespace BCSRecTests
 
 		}
 
+		//This function validates the getArea() using a length of 80 and a width of 33 (within the range)
+		//This function is expected to use the formula length*width=area or 80*33=2640
 		TEST_METHOD(AreaFunctionality_Length80Width33_Returns2640)
 		{
 			int result = 0;
